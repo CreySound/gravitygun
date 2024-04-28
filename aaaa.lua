49cd6ff8ccf0be57e1d1f6c6c20d1cdb8860dd5a
CreySound <87947166+CreySound@users.noreply.github.com>
2024-04-28 14:58:17 -0400

    Update aaaa.lua

--- aaaa.lua
+++ aaaa.lua
@@ -35,7 +35,7 @@ local cmds2 = _G.prefix."walkto <user>, ".._G.prefix.."forward <seconds>, ".._G.
 local cnds3 = .._G.prefix.."sit, ".._G.prefix.."reset, ".._G.prefix.."dance, ".._G.prefix.."undance, ".._G.prefix.."lay, ".._G.prefix.."unlay, ".._G.prefix.."damage <val>, ".._G.prefix.."stun, ".._G.prefix.."unstun, ".._G.prefix.."spin <val>, ".._G.prefix.."unspin, ".._G.prefix.."bang <speed>, "
 local cmds4 = _G.prefix.."unbang, ".._G.prefix.."fps, ".._G.prefix.."roast, ".._G.prefix.."rzz, ".._G.prefix.."rickroll, ".._G.prefix.."flipcoin, ".._G.prefix.."dice, ".._G.prefix.."freeze, ".._G.prefix.."unfreeze"
 
-local _version = 2.451
+local _version = 2.46
 
 local StarterGui = game:GetService("StarterGui")
 StarterGui:SetCore("SendNotification",{
